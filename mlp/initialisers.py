448ff87bf45d77d50c1318b165d7fef77089e5c4
Ryotaro "Justin" Miura <m_ryoppippi@outlook.com>
2017-10-31 09:32:02 +0000

    Finish!!

--- mlp/initialisers.py
+++ mlp/initialisers.py
@@ -81,8 +81,50 @@ class SELUInit(object):
         self.rng = rng
 
     def __call__(self, shape):
-        std = self.gain * (2. / shape[0])**0.5
+        std = self.gain * (1. / shape[0])**0.5
         return self.rng.normal(loc=0., scale=std, size=shape)
+
+class FanInUniformInit(object):
+
+    def __init__(self, gain=1., rng=None):
+        """Construct a normalised initilisation random initialiser object.
+        Args:
+            gain: Multiplicative factor to scale initialised weights by.
+                Recommended values is 1 for affine layers followed by
+                logistic sigmoid layers (or another affine layer).
+            rng (RandomState): Seeded random number generator.
+        """
+        self.gain = gain
+        if rng is None:
+            rng = np.random.RandomState(DEFAULT_SEED)
+        self.rng = rng
+
+    def __call__(self, shape):
+        assert len(shape) == 2, (
+            'Initialiser should only be used for two dimensional arrays.')
+        half_width = self.gain * (3./shape[0])**0.5
+        return self.rng.uniform(low=-half_width, high=half_width, size=shape)
+
+class FanOutUniformInit(object):
+
+    def __init__(self, gain=1., rng=None):
+        """Construct a normalised initilisation random initialiser object.
+        Args:
+            gain: Multiplicative factor to scale initialised weights by.
+                Recommended values is 1 for affine layers followed by
+                logistic sigmoid layers (or another affine layer).
+            rng (RandomState): Seeded random number generator.
+        """
+        self.gain = gain
+        if rng is None:
+            rng = np.random.RandomState(DEFAULT_SEED)
+        self.rng = rng
+
+    def __call__(self, shape):
+        assert len(shape) == 2, (
+            'Initialiser should only be used for two dimensional arrays.')
+        half_width = self.gain * (3./shape[1])**0.5
+        return self.rng.uniform(low=-half_width, high=half_width, size=shape)
 
 
 class GlorotUniformInit(object):
@@ -93,7 +135,7 @@ class GlorotUniformInit(object):
     of a multi-layer model consisting of interleaved affine and logistic
     sigmoidal transformation layers.
     Weights are sampled from a zero-mean uniform distribution with standard
-    deviation `sqrt(2 / (input_dim * output_dim))` where `input_dim` and
+    deviation `sqrt(2 / (input_dim + output_dim))` where `input_dim` and
     `output_dim` are the input and output dimensions of the weight matrix
     respectively.
     References:
@@ -130,7 +172,7 @@ class GlorotNormalInit(object):
     of a multi-layer model consisting of interleaved affine and logistic
     sigmoidal transformation layers.
     Weights are sampled from a zero-mean normal distribution with standard
-    deviation `sqrt(2 / (input_dim * output_dim))` where `input_dim` and
+    deviation `sqrt(2 / (input_dim + output_dim))` where `input_dim` and
     `output_dim` are the input and output dimensions of the weight matrix
     respectively.
     References:
